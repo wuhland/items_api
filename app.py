21e11eea773e3bcec3f61ce8f8966c7e30617e63
David Saeger <saegeritup@gmail.com>
2020-04-28 22:37:06 -0400

    do not load db when running

--- app.py
+++ app.py
@@ -7,8 +7,6 @@ from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
-from db import db
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///data.db'
@@ -29,5 +27,6 @@ api.add_resource(UserRegister, '/register')
 
 
 if __name__ == '__main__':
+    from db import db
     db.init_app(app)
     app.run(port=5000, debug=True)  # important to mention debug=True
